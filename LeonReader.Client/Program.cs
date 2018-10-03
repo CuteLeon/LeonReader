b0ba4f3e5211453449ab00d6e928f8fd57c457bb
Leon <xiaoyankeji@hotmail.com>
2018-10-04 02:44:21 +0800

    带着娃哈哈AD钙去CLUB SOS蹦了个养生迪回来继续撸代码；

--- LeonReader.Client/Program.cs
+++ LeonReader.Client/Program.cs
@@ -9,6 +9,7 @@ namespace LeonReader.Client
 {
     static class Program
     {
+        //TODO: SQLite error (5): database is locked
         //TODO: 优化Log输出，去除频繁且无效的、增加新方法的Log
         //TODO: 基于新架构更新单元测试
 
